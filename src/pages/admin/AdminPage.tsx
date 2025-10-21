3a0fbd6a6aa2afbc94f99853eac407a251bbfaf0
Kavin T <91929982+itskavin@users.noreply.github.com>
2025-10-22 02:50:48 +0530

    Fix prettier formatting

--- src/pages/admin/AdminPage.tsx
+++ src/pages/admin/AdminPage.tsx
@@ -14,7 +14,7 @@ export function AdminPage() {
     <SubPageLayout>
       <ThinContainer>
         <Heading1>Admin tools</Heading1>
-  <Paragraph>Silly tools used to test KamiPlay! ૮₍´˶• . • ⑅ ₎ა</Paragraph>
+        <Paragraph>Silly tools used to test KamiPlay! ૮₍´˶• . • ⑅ ₎ა</Paragraph>
 
         <ConfigValuesPart />
         <BackendTestPart />
